d4b5380e32d037072983df8c8e7c9f057a2707d7
satyamvatsal <63802932+satyamvatsal@users.noreply.github.com>
2020-09-30 19:00:10 +0530

    added a basic functionality to your code
    Now you can print  n no. of times hello world you want

--- My First Piece of Python Code!!!!.py
+++ My First Piece of Python Code!!!!.py
@@ -1,2 +1,3 @@
-print ("Hello World!")
-wait = input(" ")
+print('hello world')
+user=int(input('Enter a number to print "hello world" times')) #this will accept a number 
+print("hello world"*user) # this will print hello world n no. of times user will give
